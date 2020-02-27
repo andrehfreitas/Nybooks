b66825fe64cf2a2929981aad17f3d7136e954ea0
andrehfreitas <andrehfreitas@gmail.com>
2020-02-27 00:09:01 -0300

    Implementação de Intent para chamada da BookDetailsActivity pela BooksActivity

--- app/src/main/java/br/edu/ifsp/scl/nybooks/presentation/books/BooksViewModel.kt
+++ app/src/main/java/br/edu/ifsp/scl/nybooks/presentation/books/BooksViewModel.kt
@@ -2,22 +2,41 @@ package br.edu.ifsp.scl.nybooks.presentation.books
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import br.edu.ifsp.scl.nybooks.data.ApiService
 import br.edu.ifsp.scl.nybooks.data.model.Book
+import br.edu.ifsp.scl.nybooks.data.response.BookBodyResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class BooksViewModel: ViewModel() {
 
     val booksLiveData: MutableLiveData<List<Book>> = MutableLiveData()
 
     fun getBooks(){
-        booksLiveData.value = createFakeBooks()
-    }
+        ApiService.service.getBooks().enqueue(object :Callback<BookBodyResponse> {
+            override fun onResponse(call: Call<BookBodyResponse>, response: Response<BookBodyResponse>) {
+                if (response.isSuccessful){
+                    val books: MutableList<Book> = mutableListOf()
 
+                    response.body()?.let {
+                        for (result in it.bookResults){
+                            val book = Book(
+                                title = result.bookDetailResponses[0].title,
+                                author = result.bookDetailResponses[0].author,
+                                description = result.bookDetailResponses[0].description
+                            )
+                            books.add(book)
+                        }
+                    }
+                    booksLiveData.value = books
+                }
+            }
 
-    fun createFakeBooks(): List<Book> {
-        return listOf(
-            Book("Titulo 1", "Author 1"),
-            Book("Titulo 2", "Author 2"),
-            Book("Titulo 3", "Author 3")
-        )
+            override fun onFailure(call: Call<BookBodyResponse>, t: Throwable) {
+
+            }
+
+        })
     }
 }
